9e7520445798e87abee5acb503633c219b85f518
Rushikesh Kadukar <rushikeshkadukar369@gmail.com>
2026-05-13 21:55:00 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
+
+createChat({
+  webhookUrl: 'https://rushikesh-kadukar.app.n8n.cloud/webhook/625e6932-fbfc-49d3-ae6c-e6361dd6fabc/chat',
+  target: '#n8n-chat',
+});
